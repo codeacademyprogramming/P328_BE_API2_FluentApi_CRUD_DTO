bf0f80690545dfbbe5bcc7d6dd0cea4fe70dd730
Elchin Aghayev <Elchin@code.local>
2023-06-13 21:27:28 +0400

    project added

---
+++ FirstAPI/FirstAPI/Models/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string No { get; set; }
+        public ICollection<Student> Students { get; set; }  
+    }
+}
